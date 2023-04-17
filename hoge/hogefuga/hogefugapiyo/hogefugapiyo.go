b530d40c3352675547e8ae35bff414d4b084ba4d
kyoruni <40832190+kyoruni@users.noreply.github.com>
2023-04-18 01:42:16 +0900

    :wrench: 階層をひとつ増やして説明追加

---
+++ hoge/hogefuga/hogefugapiyo/hogefugapiyo.go
@@ -0,0 +1,7 @@
+package hogefugapiyo
+
+import "github.com/kyoruni/go-internal-test/hoge/internal/hello"
+
+func SayHello() string {
+	return hello.Say()
+}
--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
 
 	"github.com/kyoruni/go-internal-test/hoge"
 	"github.com/kyoruni/go-internal-test/hoge/hogefuga"
+	"github.com/kyoruni/go-internal-test/hoge/hogefuga/hogefugapiyo"
+	"github.com/kyoruni/go-internal-test/hoge/hogefuga/hogefugapiyo/hogefugapiyopika"
 	"github.com/kyoruni/go-internal-test/hoge/internal/hello"
 )
 
 func main() {
-	fmt.Println(hoge.SayHello())     // hello ... internalの直接の親からは呼び出せる
-	fmt.Println(hogefuga.SayHello()) // hello ... internalの兄弟からは呼び出せる
-	fmt.Println(hello.Say())         // use of internal package github.com/kyoruni/go-internal-test/hoge/internal/hello not allowed ... mainはinternalの直接の親でも兄弟でもないので、呼び出せない
+	// internalの1つ上の階層と、その配下からは呼び出すことができる
+	fmt.Println(hoge.SayHello())             // hello
+	fmt.Println(hogefuga.SayHello())         // hello
+	fmt.Println(hogefugapiyo.SayHello())     // hello
+	fmt.Println(hogefugapiyopika.SayHello()) // hello
+	fmt.Println(hello.Say())                 // use of internal package github.com/kyoruni/go-internal-test/hoge/internal/hello not allowed
 }
