c67a584f891f149aea0cc40aebaa9ef558c9dc3c
kyoruni <40832190+kyoruni@users.noreply.github.com>
2023-04-18 01:20:49 +0900

    :bug: typo

--- main.go
+++ main.go
@@ -10,6 +10,6 @@ import (
 
 func main() {
 	fmt.Println(hoge.SayHello())     // hello ... internalの直接の親からは呼び出せる
-	fmt.Println(hogefuga.SayHello()) // hello ... internakの兄弟からは呼び出せる
+	fmt.Println(hogefuga.SayHello()) // hello ... internalの兄弟からは呼び出せる
 	fmt.Println(hello.Say())         // use of internal package github.com/kyoruni/go-internal-test/hoge/internal/hello not allowed ... mainはinternalの直接の親でも兄弟でもないので、呼び出せない
 }
